b32fd15ad23ea14b79430e34f1daba45ffeb40a2
superRaptor911 <raptor.inc2018@gmail.com>
2023-04-26 08:31:44 +0530

    gg9

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,37 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     VitePWA({
-      registerType: "autoUpdate",
       devOptions: {
         enabled: true,
       },
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.png",
+        "robots.txt",
+        "apple-touch-icon.png",
+        "icons/*.svg",
+        "models/",
+      ],
+      manifest: {
+        name: "TryON",
+        short_name: "TryOn",
+        description: "Try on",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      srcDir: "./",
     }),
     react(),
   ],
